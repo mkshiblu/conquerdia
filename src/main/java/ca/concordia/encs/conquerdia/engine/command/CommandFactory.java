3ba5d42f3c9ff73eb450e00562b51715e93639ff
Mehran Jodavi <mehran.jodavi@gmail.com>
2019-09-27 18:48:08 -0300

    Implement editcontinent Command

--- src/main/java/ca/concordia/encs/conquerdia/engine/command/CommandFactory.java
+++ src/main/java/ca/concordia/encs/conquerdia/engine/command/CommandFactory.java
@@ -8,6 +8,43 @@ import java.util.List;
 
 public class CommandFactory {
     public List<Command> getCommands(String userInput, ConquerdiaModel conquerdiaModel) {
+
+        try {
+            String[] inputCommandParts = userInput.trim().split(" ");
+            int cursor = 0;
+            if (inputCommandParts.length > 2) {
+                List<Command> commands = new ArrayList<>();
+                switch (inputCommandParts[cursor++]) {
+                    case "editcontinent":
+                        while (cursor < inputCommandParts.length) {
+                            switch (inputCommandParts[cursor++]) {
+                                case ("-add"): {
+                                    String continentName = inputCommandParts[cursor++];
+                                    String continentValue = inputCommandParts[cursor++];
+                                    Continent continent = new Continent.Builder(continentName)
+                                            .setValue(continentValue)
+                                            .setWorldMap(conquerdiaModel.getWorldMap())
+                                            .build();
+                                    commands.add(continent::addContinentToWorldMap);
+                                }
+                                case "-remove": {
+                                    String continentName = inputCommandParts[cursor++];
+                                    Continent continent = new Continent.Builder(continentName)
+                                            .setWorldMap(conquerdiaModel.getWorldMap())
+                                            .build();
+                                    commands.add(continent::removeContinentFromWorldMap);
+                                }
+                            }
+                        }
+                        break;
+                }
+                return commands;
+            }
+
+        } catch (Exception ex) {
+            //TODO: Log Exception
+            String msg = ex.getMessage();
+        }
         List<Command> commands = new ArrayList<>();
         commands.add(() -> "Invalid Command");
         return commands;
